2f48743e8d4cf8945778b1f7f7e077d6f1af08ec
Martmists <martmists@gmail.com>
2018-08-12 12:56:43 +0200

    shortcode
    Signed-off-by: Martmists <martmists@gmail.com>

--- src/main/kotlin/me/ry00001/jbot/utils/OwnerChecker.kt
+++ src/main/kotlin/me/ry00001/jbot/utils/OwnerChecker.kt
@@ -1,11 +1,10 @@
 package me.ry00001.jbot.utils
 
 import me.ry00001.jbot.core.CommandContext
-import java.util.stream.LongStream
 
-class OwnerChecker {
+object OwnerChecker {
 
     fun isOwner(ctx: CommandContext): Boolean {
-        return LongStream.of(*ctx.bot.config.owners!!).anyMatch { x -> x == ctx.author.idLong }
+        return ctx.bot.config.owners.contains(ctx.author.idLong)
     }
 }
