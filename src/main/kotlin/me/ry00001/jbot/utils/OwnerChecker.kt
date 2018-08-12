8d26bf1e7170a1fbc94277bea83a0fbf504e4fc9
Martmists <martmists@gmail.com>
2018-08-12 13:03:23 +0200

    whops null
    Signed-off-by: Martmists <martmists@gmail.com>

--- src/main/kotlin/me/ry00001/jbot/utils/OwnerChecker.kt
+++ src/main/kotlin/me/ry00001/jbot/utils/OwnerChecker.kt
@@ -5,6 +5,6 @@ import me.ry00001.jbot.core.CommandContext
 object OwnerChecker {
 
     fun isOwner(ctx: CommandContext): Boolean {
-        return ctx.bot.config.owners.contains(ctx.author.idLong)
+        return ctx.bot.config.owners!!.contains(ctx.author.idLong)
     }
 }
